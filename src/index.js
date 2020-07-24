5469e223311707b3e124f0347959f9c7ae357b61
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-24 10:43:01 -0500

    Implemented Atatus

--- src/index.js
+++ src/index.js
@@ -12,5 +12,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 atatus.config('25768153e02f4a5288ea215e067bec2f').install();
-
-atatus.notify(new Error('Test Atatus Setup'));
